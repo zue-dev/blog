0d736c363c0d979c0346ed214cbbdf977948e4a2
zue-dev <zue@kakao.com>
2022-02-13 15:27:33 +0900

    feat: 안쓰는 파일 정리, 홈 스타일 모듈 분리

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import { getIssues, IRepositoryNode } from "../lib/api";
+import s from "../styles/home.module.scss";
 
 const Home: NextPage = () => {
   const [posts, setPosts] = useState<any>([]);
@@ -22,17 +23,14 @@ const Home: NextPage = () => {
       <Head>
         <title>zue</title>
         <meta property="og:title" content="zue log" />
-        <meta
-          property="og:url"
-          content="https://blog-delta-cyan.vercel.app/_next/image?url=%2Fimages%2Fmemoji2.png&w=640&q=75"
-        />
+        <meta property="og:url" content="blah" />
         <meta property="og:type" content="blog" />
         <meta property="og:image" content="/images/memoji2" />
         <meta property="og:site_name" content="zue_log" />
         <meta property="og:description" content="자유붕방 블로그" />
       </Head>
       <section>
-        <article className="text-center">
+        <article className={s.image}>
           <Image
             src="/images/memoji2.png"
             alt="profile"
@@ -40,34 +38,35 @@ const Home: NextPage = () => {
             height={200}
             className="rounded-full"
           />
-          {/* <p className="font-bold">zue log</p> */}
         </article>
-        <article className="my-16">
+        <article className={s.content}>
           {posts?.map((issue: IRepositoryNode) => (
             <Link
               key={issue.node.number}
               href={`/${encodeURIComponent(issue.node.number)}`}
             >
-              <a className="mb-10 flex justify-between last:mb-0">
-                <div className="shrink grow basis-0 pr-4">
-                  <p className="mb-1 break-words text-xl font-bold">
-                    {issue.node.title}
-                  </p>
-                  <p className="text-xs text-gray-400">
+              <a className={s.link}>
+                <div className={s.titleWrap}>
+                  <p className={s.title}>{issue.node.title}</p>
+                  <p className={s.date}>
                     {issue.node.createdAt.toString().slice(0, 10)}
                   </p>
                 </div>
-                {issue.node.comments.totalCount ? (
-                  <div className="flex items-center">
-                    <Image
-                      // TODO: bell 말고 깃헙 댓글 아이콘으로 변경하고싶음
-                      src="/images/bell.png"
-                      alt="comment"
-                      width={24}
-                      height={24}
-                    />
-                  </div>
-                ) : null}
+                <div className={s.iconWrap}>
+                  {issue.node.comments.totalCount ? (
+                    <>
+                      <Image
+                        src="/images/chatbox-outline.svg"
+                        alt="comment"
+                        width={20}
+                        height={20}
+                      />
+                      <span className={s.count}>
+                        {issue.node.comments.totalCount}
+                      </span>
+                    </>
+                  ) : null}
+                </div>
               </a>
             </Link>
           ))}
